fc9825eeb9ebf38a1c7f544f092277013e2fc382
nNzzz <nn986500@163.com>
2018-07-19 22:39:38 +0800

    vchart

---
+++ static/js/4.f5cc3d51a4ef4f21b86a.js
@@ -0,0 +1,2 @@
+webpackJsonp([4],{"2RPi":function(t,e,a){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var r={render:function(){var t=this.$createElement;return(this._self._c||t)("ve-waterfall",{attrs:{data:this.chartData}})},staticRenderFns:[]},n=a("VU/8")({data:function(){return{chartData:{columns:["活动","时间"],rows:[{"活动":"吃饭","时间":4},{"活动":"睡觉","时间":10},{"活动":"打豆豆","时间":5}]}}}},r,!1,null,null,null);e.default=n.exports}});
+//# sourceMappingURL=4.f5cc3d51a4ef4f21b86a.js.map
